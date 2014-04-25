aee824b9bb8b342822f065a200c380189abd1ce5
Yuan Lu <dianjchun@hotmail.com>
2014-04-25 16:42:28 -0400

    Update animate.js

--- animate.js
+++ animate.js
@@ -15,3 +15,116 @@ if (!window.cancelAnimationFrame) {
                                  window.oCancelAnimationFrame || window.oCancelRequestAnimationFrame ||
                                  window.clearTimeout);
 }
+
+window.animate = {};
+window.animate.captureMouse = function (element) {
+  var mouse = {x: 0, y: 0, prevX: 0, prevY: 0, event: null, move: false},
+      body_scrollLeft = document.body.scrollLeft,
+      element_scrollLeft = document.documentElement.scrollLeft,
+      body_scrollTop = document.body.scrollTop,
+      element_scrollTop = document.documentElement.scrollTop,
+      offsetLeft = element.offsetLeft,
+      offsetTop = element.offsetTop;
+  
+  element.addEventListener('mouseover', function(event) {
+    var x, y;
+    if (event.pageX || event.pageY) {
+      x = event.pageX;
+      y = event.pageY;
+    } else {
+      x = event.clientX + body_scrollLeft + element_scrollLeft;
+      y = event.clientY + body_scrollTop + element_scrollTop;
+    }
+    x -= offsetLeft;
+    y -= offsetTop;
+    
+    mouse.x = x;
+    mouse.y = y;
+  }, false);
+  
+  element.addEventListener('mousemove', function (event) {
+    var x, y;
+    if (event.pageX || event.pageY) {
+      x = event.pageX;
+      y = event.pageY;
+    } else {
+      x = event.clientX + body_scrollLeft + element_scrollLeft;
+      y = event.clientY + body_scrollTop + element_scrollTop;
+    }
+    x -= offsetLeft;
+    y -= offsetTop;
+    
+    mouse.prevX = mouse.x;
+    mouse.prevY = mouse.y;
+    mouse.x = x;
+    mouse.y = y;
+    mouse.event = event;
+    mouse.move = true;
+  }, false);
+  
+  return mouse;
+}
+
+
+window.animate.captureTouch = function (element) {
+  var touch = {x: null, y: null, prevX: 0, prevY: 0, 
+      isPressed: false, event: null, move: false},
+      body_scrollLeft = document.body.scrollLeft,
+      element_scrollLeft = document.documentElement.scrollLeft,
+      body_scrollTop = document.body.scrollTop,
+      element_scrollTop = document.documentElement.scrollTop,
+      offsetLeft = element.offsetLeft,
+      offsetTop = element.offsetTop;
+
+  element.addEventListener('touchstart', function (event) {
+    touch_event = event.touches[0]; //first touch
+    
+    if (touch_event.pageX || touch_event.pageY) {
+      x = touch_event.pageX;
+      y = touch_event.pageY;
+    } else {
+      x = touch_event.clientX + body_scrollLeft + element_scrollLeft;
+      y = touch_event.clientY + body_scrollTop + element_scrollTop;
+    }
+    
+    x -= offsetLeft;
+    y -= offsetTop;
+    
+    touch.x = x;
+    touch.y = y;
+    touch.isPressed = true;
+    touch.event = event;
+  }, false);
+
+  element.addEventListener('touchend', function (event) {
+    touch.isPressed = false;
+    touch.x = null;
+    touch.y = null;
+    touch.event = event;
+  }, false);
+  
+  element.addEventListener('touchmove', function (event) {
+    var x, y,
+        touch_event = event.touches[0]; //first touch
+    
+    if (touch_event.pageX || touch_event.pageY) {
+      x = touch_event.pageX;
+      y = touch_event.pageY;
+    } else {
+      x = touch_event.clientX + body_scrollLeft + element_scrollLeft;
+      y = touch_event.clientY + body_scrollTop + element_scrollTop;
+    }
+    
+    x -= offsetLeft;
+    y -= offsetTop;
+    
+    touch.prevX = touch.x;
+    touch.prevY = touch.y;
+    touch.x = x;
+    touch.y = y;
+    touch.event = event;
+    touch.move = true;
+  }, false);
+  
+  return touch;
+}
